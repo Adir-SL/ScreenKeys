b96f5b3ea4608d89906ddfee90b65039e1e37cab
Adir <slutzkis@gmail.com>
2022-06-13 11:59:57 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,10 +1,15 @@
+function styleKeys(){
+    sk = document.getElementById("keysDisplay").style;
+    sk.backgroundColor = "#f4f4f4";
+    sk.position = "absolute";
+    sk.padding = "8px";
+    sk.borderRadius = "8px";
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementsByTagName("body")[0].innerHTML += "<div id='keysDisplay'></div>";
+    styleKeys();
  }, false);
-
- function styleKeys(){
-     
- }
 
 window.addEventListener('keydown', function(event) {
     console.log(event.key);
