7f7af853b9f47809ca015ab7f586c5cba111af3f
Adir <slutzkis@gmail.com>
2022-06-13 11:55:53 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementsByTagName("body")[0].innerHTML += "<div id='keysDisplay'></div>";
  }, false);
 
+ function styleKeys(){
+     
+ }
+
 window.addEventListener('keydown', function(event) {
     console.log(event.key);
     document.getElementById("keysDisplay").innerText = event.key;
