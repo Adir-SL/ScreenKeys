002dda50a25ea29151769146f0d2eadf664a3b71
Adir <slutzkis@gmail.com>
2022-06-13 11:47:43 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,3 +1,6 @@
-function showKeys(posiiton, delay){
-    
+function showKeys(evt, posiiton, delay){
+    evt = evt || window.event;
+    var charCode = evt.keyCode || evt.which;
+    var charStr = String.fromCharCode(charCode);
+    alert(charStr);
 }
