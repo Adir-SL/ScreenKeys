6ac8f23b76d2ed3d9ff1e1547d578c7c13390a78
Adir <slutzkis@gmail.com>
2022-06-14 11:02:28 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
  }, false);
 
 window.addEventListener('keydown', function(event) {
+    document.getElementById("keysDisplay").style.opacity = "1";
     console.log(event.key);
     document.getElementById("keysDisplay").innerText = event.key;
     setTimeout(function(){ document.getElementById("keysDisplay").style.opacity = "0"; }, 1000);
