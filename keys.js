952b793ffcbe8103b5aed36229f376e5efde45e1
Adir <slutzkis@gmail.com>
2022-06-14 10:54:59 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,11 +1,13 @@
 function styleKeys(){
     sk = document.getElementById("keysDisplay").style;
+    sk.all = "unset";
     sk.backgroundColor = "#f4f4f4";
     sk.position = "absolute";
     sk.left = "50%";
     sk.transform = "translate(-50%, 0)";
     sk.padding = "8px";
     sk.borderRadius = "8px";
+    sk.backgroundColor = "#f4f4f4";
 }
 
 document.addEventListener('DOMContentLoaded', function() {
