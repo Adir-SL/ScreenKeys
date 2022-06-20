01dc225d831b12f501a1d772abd518c1fe289d8a
Adir <slutzkis@gmail.com>
2022-06-20 10:09:40 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -46,7 +46,7 @@ function initKeys(size, posx, posy){
     styleKeys(size, posx, posy);
     window.addEventListener('keydown', function(event) {
         document.getElementById("keysDisplay").style.opacity = "1";
-        console.log(event.key);
+        // console.log(event.key);
         if(document.getElementById("keysDisplay").innerText == ''){
             document.getElementById("keysDisplay").innerText = event.key;
         }else{
