e42d604ec0b982955e502aeeb9dbc89335a000ed
Adir <slutzkis@gmail.com>
2022-06-23 12:41:36 +0300

    Update keys.js

--- keys.js
+++ keys.js
@@ -46,6 +46,7 @@ function initKeys(size, posx, posy, delay){
     // size referes to the font-size (in em);
     // posx is the position in the x axis, can be 'left', 'right' or 'center';
     // posy is the position in the y axis, can be 'top', 'bottom' or 'center';
+    // delay refers to the time (in milliseconds) it takes for the toast to disappear;
 
     document.getElementsByTagName("body")[0].innerHTML += "<div id='keysDisplay'></div>";
     styleKeys(size, posx, posy);
